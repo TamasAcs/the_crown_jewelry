dc2befbc917ec2a0da6006ca0bc4bace775155d9
Tamas <acs.t@yahoo.com>
2022-01-28 18:00:28 +0000

    Buttons for filter

---
+++ src/Components/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Button.css";
+
+function Button({ button, filter }) {
+  return (
+    <div className="buttons">
+      {button.map((cat, i) => {
+        return (
+          <button type="button" onClick={() => filter(cat)} className="btn">
+            {cat}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Button;
