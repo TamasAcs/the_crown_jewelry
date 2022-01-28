7b2e16b92503120ebd2e43b08d767ad0477e0ea2
Tamas <acs.t@yahoo.com>
2022-01-28 17:59:30 +0000

    Navbar section

---
+++ src/Components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
+import logo from "../img/logo2.png";
+import './Navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-links_logo">
+          <img src={logo} alt=""/>
+        </div>
+        <div className="navbar-links_container">
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Jewelry</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+      <div className="navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="navbar-menu_container scale-uLink-center">
+          <div className="navbar-menu_container-links">
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Jewelry</Link>
+          <Link to="/contact">Contact</Link>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
