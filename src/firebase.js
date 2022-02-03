186df47ba86d546ab9f1a6f472fcc11c27ad7bd4
Tamas <acs.t@yahoo.com>
2022-02-03 02:14:00 +0000

    Connected with firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJcUmk68sO46DubgOa1mwsZjUhWgyP3U4",
+  authDomain: "the-crown-jewelry.firebaseapp.com",
+  projectId: "the-crown-jewelry",
+  storageBucket: "the-crown-jewelry.appspot.com",
+  messagingSenderId: "501164651559",
+  appId: "1:501164651559:web:1097908fe8ec169baeca02",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
